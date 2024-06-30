668c61e960572926da7ffaf2cadd919af955fc7a
Pedro Marins <ph.marins@hotmail.com>
2024-06-29 23:39:23 -0300

    feat: implement logging

--- payment/adapters/controllers/get_payment_controller.py
+++ payment/adapters/controllers/get_payment_controller.py
@@ -3,6 +3,7 @@ Get payment from DynamoDB
 """
 import json
 import os
+import logging
 import mercadopago
 
 from payment.adapters.repositories.dynamo_db_payment_repository import DynamoDBPaymentRepository
@@ -10,21 +11,38 @@ from payment.application.services.payment_service import PaymentService
 
 sdk = mercadopago.SDK(os.environ.get("ENV_ACCESS_TOKEN"))
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger(__name__)
+
 def get_payment(event, context):
     """
     Get payment from DynamoDB
     """
-    body = json.loads(event["body"])
-    idempotency_key = body["idempotency_key"]
+    logger.info("Getting payment")
+    try:
+        body = json.loads(event["body"])
+        idempotency_key = body["idempotency_key"]
 
-    payment_repository = DynamoDBPaymentRepository()
-    payment_service = PaymentService(payment_repository)
+        payment_repository = DynamoDBPaymentRepository()
+        payment_service = PaymentService(payment_repository)
 
-    response = payment_repository.get_payment_by_idempotency_key(idempotency_key)
-    item = response["Item"]
-    prettified_response = payment_service.pretty_print_payment(item)
+        response = payment_repository.get_payment_by_idempotency_key(idempotency_key)
+        item = response["Item"]
+        prettified_response = payment_service.pretty_print_payment(item)
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps(prettified_response)
-    }
+        return {
+            "statusCode": 200,
+                "body": json.dumps(prettified_response)
+            }
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        return {
+            'statusCode': 500
+        }
+    finally:
+        logger.info("Payment retrieved")
--- payment/adapters/controllers/pay_controller.py
+++ payment/adapters/controllers/pay_controller.py
@@ -1,23 +1,43 @@
 """Payment controller"""
 import json
+import logging
+from pydantic import ValidationError
 
 from payment.adapters.repositories.dynamo_db_payment_repository import DynamoDBPaymentRepository
 from payment.application.services.payment_service import PaymentService
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger(__name__)
+
 def create_payment(event, context):
     """Payment controller"""
-    body = json.loads(event["Records"][0]["body"])
-    vehicle_id = body["vehicle_id"]
-    order_id = body["order_id"]
-    idempotency_key = body["idempotency_key"]
+    logger.info("Starting payment")
+    try:
+        body = json.loads(event["Records"][0]["body"])
+        vehicle_id = body["vehicle_id"]
+        order_id = body["order_id"]
+        idempotency_key = body["idempotency_key"]
 
-    payment_repository = DynamoDBPaymentRepository()
-    payment_service = PaymentService(payment_repository)
-    payment = payment_service.start_payment(idempotency_key, vehicle_id)
+        payment_repository = DynamoDBPaymentRepository()
+        payment_service = PaymentService(payment_repository)
+        payment = payment_service.start_payment(idempotency_key, vehicle_id)
 
-    payment_service.create_payment({
-        **payment,
-        "idempotency_key": idempotency_key,
-        "order_id": order_id,
-        "vehicle_id": vehicle_id
-    })
+        payment_service.create_payment({
+            **payment,
+            "idempotency_key": idempotency_key,
+            "order_id": order_id,
+            "vehicle_id": vehicle_id
+        })
+
+        logging.info(f"Payment started with {idempotency_key} and order {order_id}")
+
+    except ValidationError as e:
+        logger.error(f"Validation error: {e}")
+
+    except Exception as e:
+        logger.error(f"Error: {e}")
